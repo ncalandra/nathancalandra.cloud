930379a19513efe4692ecb0474d50cd23685b94d
Nathan Calandra <njcalandra@gmail.com>
2019-11-09 16:56:15 -0500

    Format layernames

--- src/pages/goes16serverless/OverlaySelector.js
+++ src/pages/goes16serverless/OverlaySelector.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+// MomentJS
+import moment from 'moment'
+
 // react-bootstrap imports
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
-
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -12,7 +14,7 @@ function OverlaySelector(props) {
   return (
     <Container fluid={true} style={{paddingTop: '15px'}}>
       <Row>
-        <Col md={{ span: 4, offset: 8 }} xs={{ span: 12, offset: 0 }}>
+        <Col md={{ span: 3, offset: 9 }} xs={{ span: 12, offset: 0 }}>
           <Card bg="dark" text="white">
             <Card.Header style={{ textAlign: 'center', padding: '0' }}>Overlays</Card.Header>
             <Card.Body style={{ padding: '0.75rem' }}>
@@ -22,9 +24,17 @@ function OverlaySelector(props) {
                   onChange={props.updateOverlay}
                 >
                   {props.layers.map(layer => {
+
+                    const time = moment.utc(
+                      layer.name.split('_')[4].slice(1),
+                      'YYYYDDDhhmmss'
+                    ).local().format('LLL');
+
+                    const product = layer.name.split('/').slice(-1)[0].split('_')[1];
+
                     return (
                       <option key={layer.name} value={layer.name}>
-                        {layer.name.split('/').slice(-1)[0]}
+                        {product + ': ' + time}
                       </option>
                     );
                   })}
